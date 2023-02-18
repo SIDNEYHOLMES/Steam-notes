3bc47cff23270ef1cf7e2da48202b3dbd7d6498f
SIDNEYHOLMES <sidneyselassie1@gmail.com>
2023-02-17 19:08:32 -0600

    user schema

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path')
+const {MongoClient} = require('mongodb');
 const session = require('express-session')
 const rootRouter = require('./routes');
 const passport = require('passport');
